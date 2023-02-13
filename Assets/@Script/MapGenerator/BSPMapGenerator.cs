d2532ed9e3a79ed63b3c40ead7854e2763eb7f36
dkdlelrktlfj <dkdlelrktlfj@naver.com>
2023-02-14 00:54:40 +0900

    Ahri
    BSP 수정 및 디버거 수정

--- Assets/@Script/MapGenerator/BSPMapGenerator.cs
+++ Assets/@Script/MapGenerator/BSPMapGenerator.cs
@@ -23,12 +23,30 @@ public class BSPMapGenerator
 
         //트리를 분할하여 방을 만드는 과정
         DivideBinaryTree(roomNode, 0, _mapData);
-
+        GenerateDungeon(roomNode, 0, _mapData);
     }
 
-    public void GenerateDungeon()
+    private RectInt GenerateDungeon(RoomNode _root, int _n, MapData _mapData)
     {
+        var debugger = MapDebugger.Instance;
+        if (_n == _mapData.maxNode)
+        {
+            RectInt size = _root.treeSize;
+            int width = Mathf.Max(UnityEngine.Mathf.Max(size.width / 2, size.width - 1));
+            int height = Mathf.Max(UnityEngine.Mathf.Max(size.height / 2, size.height - 1));
 
+            int x = _root.treeSize.x + UnityEngine.Random.Range(1, size.width - width);
+            int y = _root.treeSize.y + UnityEngine.Random.Range(1, size.height - height);
+
+            var rect = new RectInt(x, y, width, height);
+            debugger.AddRect(rect);
+            return rect;
+        }
+
+        ((RoomNode)_root.leftLeaf).roomSize = GenerateDungeon(_root.leftLeaf as RoomNode, _n + 1, _mapData);
+        ((RoomNode)_root.rightLeaf).roomSize = GenerateDungeon(_root.rightLeaf as RoomNode, _n + 1, _mapData);
+
+        return ((RoomNode)_root.rightLeaf).roomSize;
     }
 
     private void DivideBinaryTree(RoomNode _rootNode, int _n, MapData _mapData)
@@ -37,7 +55,7 @@ public class BSPMapGenerator
         {
             var debugger = MapDebugger.Instance;
 
-            RectInt size = _rootNode.roomSize;
+            RectInt size = _rootNode.treeSize;
             int length = size.width >= size.height ? size.width : size.height; // 트리의 기준선 작업, 세로 또는 가로 중 긴 축을 사용
             int split = Mathf.RoundToInt(UnityEngine.Random.Range(length * _mapData.minDivideSize, length * _mapData.maxDivideSize));
 
@@ -48,7 +66,7 @@ public class BSPMapGenerator
                 _rootNode.leftLeaf = new RoomNode(size.x, size.y, split, size.height);
 
                 //x 값에 split값을 더하여 좌표를 설정, 이전 트리의 width값에 split값을 빼 가로 길이를 설정
-                _rootNode.rightLeaf = new RoomNode(size.x * split, size.y, size.width - split, size.height);
+                _rootNode.rightLeaf = new RoomNode(size.x + split, size.y, size.width - split, size.height);
 
                 debugger.AddLine(new DebugLine(new Vector2Int(size.x + split, size.y), new Vector2Int(size.x + split, size.y + size.height)));
             }
@@ -56,7 +74,7 @@ public class BSPMapGenerator
             else
             {
                 _rootNode.leftLeaf = new RoomNode(size.x, size.y, size.width, split);
-                _rootNode.leftLeaf = new RoomNode(size.x, size.y * split, size.width, size.height - split);
+                _rootNode.rightLeaf = new RoomNode(size.x, size.y + split, size.width, size.height - split);
                 debugger.AddLine(new DebugLine(new Vector2Int(size.x, size.y + split), new Vector2Int(size.x + size.width, size.y + split)));
             }
 
@@ -67,17 +85,6 @@ public class BSPMapGenerator
             //재귀 호출로 _mapData의 maxNode값까지 트리 분할
             DivideBinaryTree(_rootNode.leftLeaf as RoomNode, _n + 1, _mapData);
             DivideBinaryTree(_rootNode.rightLeaf as RoomNode, _n + 1, _mapData);
-        }
-    }
-
-    public void GenerateDungeon(RoomNode _rootNode, int _n, MapData _mapdata)
-    {
-        if (_n == _mapdata.maxNode)
-        {
-            RectInt size = _rootNode.treeSize;
-
-            int width = Mathf.Max(UnityEngine.Random.Range(size.width / 2, size.width - 1));
-            int height = Mathf.Max(UnityEngine.Random.Range(size.height / 2, size.height - 1));
         }
     }
 }
--- Assets/@Script/MapGenerator/MapDebugger.cs
+++ Assets/@Script/MapGenerator/MapDebugger.cs
@@ -16,28 +16,41 @@ public struct DebugLine
 }
 public class MapDebugger : MonoSingleton<MapDebugger>
 {
+    public MapData data;
+
     private List<DebugLine> lines = new List<DebugLine>(80);
-
-    private void Start()
-    {
-        MapData data = new MapData();
-        data.dungeonSize = new Vector2Int(100, 100);
-        data.minDivideSize = 1;
-        data.maxDivideSize = 5;
-
-        data.maxNode = 5;
-
-        BSPMapGenerator generator = new BSPMapGenerator(data);
-    }
+    private List<RectInt> tiles = new List<RectInt>(80);
 
     private void Update()
     {
-        if(lines.Count > 0)
+        Debug.DrawLine(new Vector3(0,0,0), new Vector3(0, data.dungeonSize.y, 0), Color.blue);
+        Debug.DrawLine(new Vector3(0, 0, 0), new Vector3(data.dungeonSize.x, 0, 0), Color.red);
+        Debug.DrawLine(new Vector3(data.dungeonSize.x, 0, 0), new Vector3(data.dungeonSize.x, data.dungeonSize.y, 0), Color.yellow);
+        Debug.DrawLine(new Vector3(0, data.dungeonSize.y, 0), new Vector3(data.dungeonSize.x, data.dungeonSize.y, 0), Color.green);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            lines.Clear();
+            tiles.Clear();
+            BSPMapGenerator generator = new BSPMapGenerator(data);
+        }
+
+        if (lines.Count > 0)
         {
             for(int index = 0; index < lines.Count; index++)
             {
                 var current = lines[index];
-                Debug.DrawLine(new Vector3(current.from.x, current.from.y, 0), new Vector3(current.to.x, current.to.y, 0));
+                Debug.DrawLine(
+                    new Vector3(current.from.x, current.from.y, 0),
+                    new Vector3(current.to.x, current.to.y, 0));
+            }
+        }
+
+        if(tiles.Count > 0)
+        {
+            for (int index = 0; index < tiles.Count; index++)
+            {
+                DrawRect(tiles[index], Color.red);
             }
         }
     }
@@ -45,5 +58,23 @@ public class MapDebugger : MonoSingleton<MapDebugger>
     public void AddLine(DebugLine _line)
     {
         lines.Add(_line);
+    }
+
+    public void AddRect(RectInt _rect)
+    {
+        tiles.Add(_rect);
+    }
+
+    private void DrawRect(RectInt rect, Color color)
+    {
+        DrawRect(new Vector3(rect.min.x, rect.min.y, 0), new Vector3(rect.max.x, rect.max.y, 0), color);
+    }
+
+    private void DrawRect(Vector3 min, Vector3 max, Color color)
+    {
+        UnityEngine.Debug.DrawLine(min, new Vector3(min.x, max.y), color);
+        UnityEngine.Debug.DrawLine(new Vector3(min.x, max.y), max, color);
+        UnityEngine.Debug.DrawLine(max, new Vector3(max.x, min.y), color);
+        UnityEngine.Debug.DrawLine(min, new Vector3(max.x, min.y), color);
     }
 }
